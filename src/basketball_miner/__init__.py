69fdfdf74f5b864a4497421541de1950f4ff8960
이현준 <110889799+Rudwpahs@users.noreply.github.com>
2026-09-09 23:42:39 +0900

    feat: add basketball miner package

---
+++ src/basketball_miner/__init__.py
@@ -0,0 +1,5 @@
+"""Basketball Knowledge Miner public collector package."""
+
+from .models import CandidateRecord, Checkpoint, RunCounters, SourceRecord
+
+__all__ = ["CandidateRecord", "Checkpoint", "RunCounters", "SourceRecord"]
